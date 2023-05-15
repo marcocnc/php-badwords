74038c8b2dbc38d9ef7849e87250b1e2162c2683
marcocnc <marcocancelliere56@gmail.com>
2023-05-15 14:45:24 +0200

    modified form and printed data in php file

---
+++ landing-page-form.php
@@ -0,0 +1,33 @@
+<?php
+
+$email = $_GET['email'];
+$password = $_GET['Password'];
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing page</title>
+</head>
+<body>
+    <div class="container">
+        <ul>
+
+            <li>
+                <?php echo $email?>
+            </li>
+            
+            
+            <li>
+                <?php echo $password?>
+            </li>
+        </ul>
+    </div>
+</body>
+</html>
